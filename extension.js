04948e4fa62d0cd4c57516796a119fca3b84d993
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2023-09-25 10:07:32 +0200

    Port extension to GNOME 45.
    Version 45 requires changes from extension developers:
      https://gjs.guide/extensions/upgrading/gnome-shell-45.html
    
    In particular, we update this extension to use ES modules and the
    Extension base class.
    
    Indentation is kept incorrect on purpose, to focus this patch on the
    actual changes required to port the extension.

--- extension.js
+++ extension.js
@@ -1,26 +1,27 @@
-const { GLib, St, Clutter, Pango } = imports.gi;
-const main = imports.ui.main;
-const ExtensionUtils = imports.misc.extensionUtils;
+import GLib from 'gi://GLib';
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+import Pango from 'gi://Pango';
+
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as main from 'resource:///org/gnome/shell/ui/main.js';
 
 let originalClockDisplay;
 let formatClockDisplay;
 let settings;
 let timeoutID = 0;
 
-/**
- * Initialising function which will be invoked at most once directly after your source JS file is loaded.
- */
-function init() {}
+export default class PanelDateFormatExtension extends Extension {
 
 /**
  * Enable, called when extension is enabled or when screen is unlocked.
  */
-function enable() {
+enable() {
   originalClockDisplay = main.panel.statusArea.dateMenu._clockDisplay;
   formatClockDisplay = new St.Label({ style_class: "clock" });
   formatClockDisplay.clutter_text.y_align = Clutter.ActorAlign.CENTER;
   formatClockDisplay.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
-  settings = ExtensionUtils.getSettings();
+  settings = this.getSettings();
 
   // FIXME: Set settings first time to make it visible in dconf Editor
   if (!settings.get_string("format")) {
@@ -37,13 +38,14 @@ function enable() {
 /**
  * Disable, called when extension is disabled or when screen is locked.
  */
-function disable() {
+disable() {
   GLib.Source.remove(timeoutID);
   timeoutID = 0;
   originalClockDisplay.get_parent().remove_child(formatClockDisplay);
   originalClockDisplay.show();
   settings = null;
   formatClockDisplay = null;
+}
 }
 
 /**
